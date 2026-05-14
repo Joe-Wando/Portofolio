502b10f290514cded98123bdaad032538e6b282d
Joe-Wando <jonathan6wando@gmail.com>
2026-05-14 13:57:32 +0000

    modification du vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  // Ajoute bien les deux slashs / autour du nom
-  base: '/Portofolio/', 
+  base: './',
   build: {
-    outDir: 'dist',
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        formations: resolve(__dirname, 'formations.html'),
+        competences: resolve(__dirname, 'competences.html'),
+        experiences: resolve(__dirname, 'experiences.html'),
+        projets: resolve(__dirname, 'projets.html'),
+        A_propos: resolve(__dirname, 'A_propos.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
 })
