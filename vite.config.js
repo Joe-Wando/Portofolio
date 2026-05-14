17d7de1f91317a0afe9a69f4001a507a8068d706
Joe-Wando <jonathan6wando@gmail.com>
2026-05-14 00:31:38 +0000

    Fix: configuration du base path pour le déploiement

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Utiliser './' permet à ton site de trouver ses fichiers (CSS, JS) 
-  // peu importe s'il est hébergé sur github.io/Portofolio ou ailleurs.
-  base: './', 
+
+  base: '/Portofolio/', 
   build: {
     outDir: 'dist',
   }
